1ead5d46e7628adc758b6605fb986ce3e0432f74
marek.bukovsky <marek.bukovsky@asseco-ce.com>
2023-07-21 11:50:17 +0200

    INT: updated package.json to universal demo

--- app/server.cjs
+++ app/server.cjs
@@ -1,5 +1,6 @@
 const connect = require('connect'),
       gzipStatic = require('connect-gzip-static'),
+      serveStatic = require('serve-static'),
       history = require('connect-history-api-fallback'),
       {createProxyMiddleware} = require('http-proxy-middleware'),
       argv = require('yargs').argv,
@@ -16,6 +17,43 @@ const app = connect();
 connectExtensions.extendConnectUse(app);
 
 const wwwroot = path.join(__dirname, 'wwwroot');
+const proxyUrlFile = path.join(__dirname, 'proxyUrl.cjs');
+var proxyUrl = "http://127.0.0.1:8080";
+
+const key = fs.readFileSync('server.key');
+const cert = fs.readFileSync('server.crt');
+
+const options = 
+{
+    key: key,
+    cert: cert
+};
+
+function isRequireAvailable(path)
+{
+    try
+    {
+        require.resolve(path);
+    }
+    catch(e)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+if(isRequireAvailable(proxyUrlFile))
+{
+    proxyUrl = require(proxyUrlFile);
+}
+
+if(process.env.SERVER_PROXY_HOST)
+{
+    proxyUrl = process.env.SERVER_PROXY_HOST;
+}
+
+console.log(`Using proxy url '${proxyUrl}'`);
 
 //enable webpack only if run with --webpack param
 if(!!argv.webpack)
@@ -30,11 +68,64 @@ if(!!argv.webpack)
     }));
 }
 
+//mock rest api
+require('./server.mock.cjs')(app);
+
+const onError = function(err, req, res)
+{
+    if(err.code == "ECONNREFUSED" || err.code == "ECONNRESET")
+    {
+        res.writeHead(503,
+        {
+            'Content-Type': 'text/plain'
+        });
+
+        res.end('Remote server is offline.');
+
+        return;
+    }
+
+    res.writeHead(504,
+    {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Failed to proxy request.');
+}
+
+//proxy special requests to other location
+app.use(createProxyMiddleware(['/api', '/swagger'],
+                              {
+                                  target: proxyUrl,
+                                  ws: true,
+                                  secure: false,
+                                  changeOrigin: true,
+                                  onError
+                              }));
+
+//custom rest api
+require('./server.rest.cjs')(app);
+
 //enable html5 routing
 app.use(history());
+
 //return static files
-app.use(gzipStatic(wwwroot));
+app.use(gzipStatic(wwwroot, 
+                   {
+                       maxAge: '7d',
+                       setHeaders: function setCustomCacheControl (res, path) 
+                       {
+                           if (serveStatic.mime.lookup(path) === 'text/html') 
+                           {
+                               // Custom Cache-Control for HTML files
+                               res.setHeader('Cache-Control', 'public, max-age=0');
+                           }
+                       }
+                   }));
 
 console.log("Listening on port 8888 => http://localhost:8888");
 //create node.js http server and listen on port
 app.listen(8888);
+console.log("Listening on port 443 => https://localhost");
+//create node.js https server and listen on port
+https.createServer(options, app).listen(443);
